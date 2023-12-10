37e1db1c69a5d02aebd7fa0face97bf9d98e8ec6
chaugiakhang193 <83385460+chaugiakhang193@users.noreply.github.com>
2023-12-10 15:38:42 +0700

    Add yaml + dockerfile

--- database/db.js
+++ database/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+const mongodbUriBase64 = process.env.MONGODB_URL
+const mongodbUri = Buffer.from(mongodbUriBase64, 'base64').toString('utf-8');
 async function connect(){
-    mongoose.connect(process.env.MONGODB_URL)
+   
+    console.log("URL " + mongodbUri);
+    mongoose.connect(mongodbUri)
         .then(() =>
         {console.log("Connected to MongoDB");}
 
